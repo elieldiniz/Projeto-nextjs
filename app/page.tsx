8ed1c1cbc9ee97337846e1dba76dff6c948939bf
elieldiniz <elielfantichelle@outlook.com.br>
2023-10-23 16:36:04 -0400

    add zod and creat form

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,14 @@
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { RedirectType, redirect } from 'next/navigation';
 
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
 import {
   Tabs,
   TabsContent,
   TabsList,
-  TabsTrigger,
+  TabsTrigger
 } from "@/components/ui/tabs"
+import { CreateAccountForm } from "@/components/auth/Create-account";
 
 export default async function Home() {
   let loggedIn = false;
@@ -34,64 +24,25 @@ export default async function Home() {
     console.error('Home', error);
   } finally {
     if (loggedIn) {
-      redirect("/user-app");
+      redirect("/user-app", RedirectType.replace)
     }
   }
 
   return (
-    <div className="flex  flex-col h-screen w-full justify-center items-center">
-        <Tabs defaultValue="create-account" className="w-[400px]">
-      <TabsList className="grid w-full grid-cols-2">
-        <TabsTrigger value="create-account">Account</TabsTrigger>
-        <TabsTrigger value="password">Password</TabsTrigger>
-      </TabsList>
-      <TabsContent value="create-account">
-        <Card>
-          <CardHeader>
-            <CardTitle>Creat your Account</CardTitle>
-            <CardDescription>
-              Make changes to your account here. Click save when you're done.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" defaultValue="Pedro Duarte" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="username">Username</Label>
-              <Input id="username" defaultValue="@peduarte" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save changes</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-      <TabsContent value="password">
-        <Card>
-          <CardHeader>
-            <CardTitle>Password</CardTitle>
-            <CardDescription>
-              Change your password here. After saving, you'll be logged out.
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-2">
-            <div className="space-y-1">
-              <Label htmlFor="current">Current password</Label>
-              <Input id="current" type="password" />
-            </div>
-            <div className="space-y-1">
-              <Label htmlFor="new">New password</Label>
-              <Input id="new" type="password" />
-            </div>
-          </CardContent>
-          <CardFooter>
-            <Button>Save password</Button>
-          </CardFooter>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <div className="flex  flex-col h-screen w-full justify-center items-center bg-blue-300">
+      <Tabs defaultValue="create-Account" className="w-[400px] border rounded-md pb-4 shadow-2xl ">
+          <TabsList className="flex justify-around items-center rounded-b-none h-14">
+              <TabsTrigger value="create-Account" className="trasition-all delay-150">Account</TabsTrigger>
+              <TabsTrigger value="password" className="trasition-all delay-150" >Login</TabsTrigger>
+          </TabsList>
+
+          <TabsContent value="create-Account">
+              <CreateAccountForm/>
+          </TabsContent>
+          <TabsContent value="login">
+
+          </TabsContent>
+      </Tabs>
     </div>
   );
 }
---
+++ components/auth/Create-account.tsx
@@ -0,0 +1,38 @@
+"user cliente"
+
+import * as z from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+
+// Definindo um esquema de validação chamado 'formSchema'
+const formSchema = z.object({
+    // Propriedade 'email' no esquema
+    email: z.string({
+        // Mensagem de erro personalizada se 'email' estiver ausente
+        required_error: 'Por favor, insira um e-mail',
+    }).email({
+        // Validação de formato de e-mail
+        message: 'E-mail inválido'
+    }),
+
+    // Propriedade 'password' no esquema
+    password: z.string({
+        // Mensagem de erro personalizada se 'password' estiver ausente
+        required_error: 'A senha é obrigatória'
+    }).min(7, {
+        // Garante que a senha tenha pelo menos 7 caracteres
+        message: 'A senha precisa ter pelo menos 7 caracteres'
+    }).max(12, {
+        // Garante que a senha tenha no máximo 12 caracteres
+        message: 'Máximo de 12 caracteres permitidos'
+    })
+});
+
+
+export function CreateAccountForm(){
+    return <h1>CreateAccount</h1>
+}
